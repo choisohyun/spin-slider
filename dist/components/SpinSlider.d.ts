bb4577260d3c80c1e0a9875412925a240591a3d4
hoolie-triple <hoolie96@nol-universe.com>
2025-07-08 22:12:16 +0900

    불필요한 파일 삭제

--- dist/components/SpinSlider.d.ts
+++
@@ -1,6 +0,0 @@
-import { SpinSliderProps } from '../types';
-/**
- * 메인 SpinSlider 컴포넌트
- */
-export declare const SpinSlider: <T>({ items, renderItem, visibleCount, sidePeek, autoPlay, autoPlayInterval, infinite, showPagination, showNavigation, minSwipeDistance, transitionDuration, initialIndex, styles, callbacks, accessibility, className, }: SpinSliderProps<T>) => import("react/jsx-runtime").JSX.Element | null;
-//# sourceMappingURL=SpinSlider.d.ts.map
